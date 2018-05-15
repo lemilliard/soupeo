48ad99a1cbe755300b9471d9a2b86f34a944bec3
Thomas Kint <thomaskint.pro@gmail.com>
2018-05-15 17:14:42 +0200

    Modif run dev

--- back/run_dev.sh
+++ back/run_dev.sh
@@ -1,2 +1,8 @@
+git clone https://github.com/LeMilliard/DecisionTree.git
+cd DecisionTree
+mvn clean install
+cd ..
+rm -rf DecisionTree
+
 cd SoupeoServer && mvn clean package
 java -jar target/soupeoserver-0.0.1-SNAPSHOT.jar
