56fe4fbdfc88a4970296d9cc1d11b74857882346
Franofarion <baptiste.domange@gmail.com>
2018-05-16 10:28:17 +0200

    DecisionTree still not found on ubuntu

--- back/run_dev.sh
+++ back/run_dev.sh
@@ -1,8 +1,12 @@
-git clone https://github.com/LeMilliard/DecisionTree.git
-cd DecisionTree
-mvn clean install
-cd ..
-rm -rf DecisionTree
-
-cd SoupeoServer && mvn clean package
-java -jar target/soupeoserver-0.0.1-SNAPSHOT.jar
+#!/bin/bash
+git clone https://github.com/LeMilliard/DecisionTree.git;
+cd DecisionTree/;
+mvn clean install;
+# ls;
+cd ..;
+# ls;
+rm -rf DecisionTree/;
+cd SoupeoServer;
+# ls;
+mvn clean package;
+java -jar target/soupeoserver-0.0.1-SNAPSHOT.jar;
