48e371e08a8f5606d190895b29bc2d074c9840cc
Thomas Kint <thomaskint.pro@gmail.com>
2018-05-17 10:12:55 +0200

    Analyse OK

--- back/SoupeoServer/src/main/java/fr/epsi/i4/soupeoserver/analyzer/Analyzer.java
+++ back/SoupeoServer/src/main/java/fr/epsi/i4/soupeoserver/analyzer/Analyzer.java
@@ -2,18 +2,18 @@ package fr.epsi.i4.soupeoserver.analyzer;
 
 import com.google.gson.internal.LinkedTreeMap;
 import fr.epsi.i4.soupeoserver.analyzer.decisiontree.PageEnum;
-import fr.epsi.i4.soupeoserver.faceapi.FaceAPIClient;
-import fr.epsi.i4.soupeoserver.model.Emotion;
-
-import java.util.ArrayList;
-
-import static fr.epsi.i4.soupeoserver.SoupeoServerApplication.analyseDecisionTree;
 import fr.epsi.i4.soupeoserver.dao.UserSessionDAO;
+import fr.epsi.i4.soupeoserver.faceapi.FaceAPIClient;
 import fr.epsi.i4.soupeoserver.mapper.Mapper;
+import fr.epsi.i4.soupeoserver.model.Emotion;
 import fr.epsi.i4.soupeoserver.model.morphia.Parcours;
 import fr.epsi.i4.soupeoserver.model.morphia.UserSession;
 import fr.epsi.i4.soupeoserver.utils.WebUtils;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static fr.epsi.i4.soupeoserver.SoupeoServerApplication.analyseDecisionTree;
 
 /**
  * @author Thomas Kint
@@ -21,7 +21,7 @@ import java.util.List;
 public class Analyzer {
 
 	public static AnalyzerResult analyze(int index, byte[] image) {
-                UserSession userSession = UserSessionDAO.getCurrentSession(WebUtils.getClientIp());
+		UserSession userSession = UserSessionDAO.getCurrentSession(WebUtils.getClientIp());
 		PageEnum pagePrecedente = getPagePrecedente(index, userSession.getParcours());
 		PageEnum pageActuelle = getPageActuelle(index, userSession.getParcours());
 		int nombreVisites = getNombreVisites(index, userSession.getParcours());
@@ -34,26 +34,26 @@ public class Analyzer {
 
 	private static PageEnum getPageActuelle(int index, List<Parcours> parcours) {
 		String url = parcours.get(index).getUrl();
-                return Mapper.urlToPageEnum(url);
+		return Mapper.urlToPageEnum(url);
 	}
 
 	private static PageEnum getPagePrecedente(int index, List<Parcours> parcours) {
-                if(index > 0){
-                    String url = parcours.get(index - 1).getUrl();
-                    return Mapper.urlToPageEnum(url);
-                } 
-                return PageEnum.PORTAIL;
+		if (index > 0) {
+			String url = parcours.get(index - 1).getUrl();
+			return Mapper.urlToPageEnum(url);
+		}
+		return PageEnum.PORTAIL;
 	}
 
 	private static int getNombreVisites(int index, List<Parcours> parcours) {
 		Parcours parcour = parcours.get(index);
-                int count = 0;
-                for (Parcours p : parcours){
-                    if(p.getUrl().equals(parcour.getUrl())){
-                        count ++;
-                    }
-                }
-                return count;
+		int count = 0;
+		for (Parcours p : parcours) {
+			if (p.getUrl().equals(parcour.getUrl())) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 	private static int getEmotionScore(byte[] image) {
